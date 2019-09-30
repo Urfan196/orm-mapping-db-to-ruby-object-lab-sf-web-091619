b2a93917692199983a5c7446be3456b1a1459b66
Urfan Guliyev <urfan196@yahoo.com>
2019-09-30 04:17:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize (id=nil, name, grade)
+    @id= id
+    @name = name
+    @grade = grade
+  end
+
   def self.new_from_db(row)
     new_student = self.new(row[0], row[1], row[2])
   # new_student.id = row[0]
